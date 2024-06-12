a80ce7efe62cffffad9c27172e1391392e359f78
Nafiur01 <nafiurrahman373@gmail.com>
2024-06-12 23:28:41 +0600

    changed caption

--- pygame1.py
+++ pygame1.py
@@ -2,7 +2,7 @@ import pygame
 pygame.init()
 
 win = pygame.display.set_mode((585,360))
-pygame.display.set_caption("First Game")
+pygame.display.set_caption("Pygame Adventure")
 
 walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
 walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
